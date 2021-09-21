42eafb1d6ca06bffb6c01c25b331d5220eaa9d10
Carol Zhang <zhang.carol19@gmail.com>
2021-09-21 13:05:53 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# return-marked-files
+
+## Usage:
+1. The `marked-files/` folder will contain files that will be uploaded to corresponding student submissions as comments.
+ Files in the `marked-files/` folder must have the following naming format: `"<firstnamelastname>_<StudentID>_<SubmissionID>_<assignment_name>.<ext>"`, 
+ such as `"johnsmith_1234567_98765432_earth_assignment.html"`.
+2. Add 
+ 
+ 
+
---
+++ return_marked_files.py
@@ -0,0 +1,79 @@
+from canvasapi import Canvas
+import yaml
+from pathlib import Path
+import os
+import sys
+
+"""
+Submission file names will have the form: 
+firstnamelastname_studentid_submissionid_assignmentname.____
+"""
+
+'''Dictionary with key as student id and corresponding submission 
+file path as value '''
+def create_marked_files_dict(path) :
+    content = os.listdir(path)
+    marked_files = {}
+
+    for f in content:
+        file_path = os.path.join(path, f)
+        
+        if os.path.isfile(file_path):
+            stu_id = f.split('_')[1]
+            marked_files[stu_id] = file_path
+
+    return marked_files
+
+'''Dictionary with key as student id and corresponding canvas submission as value '''
+def create_submissions_dict(canvas, course_id, assignment_id):
+    submissions_dict = {}
+    submissions = get_submissions(canvas, course_id, assignment_id)
+    for submission in submissions:
+        stu_id = str(submission).split('-')[1]
+        submission_id = submission
+        submissions_dict[stu_id] = submission_id
+
+    return submissions_dict
+
+
+'''Connect to Canvas'''
+def connect_to_canvas():
+    path_to_token = Path(".").resolve()
+    file = open(path_to_token / "token.yml")
+    token = yaml.load(file, Loader=yaml.FullLoader)
+
+    API_URL = "https://canvas.ubc.ca/"
+    API_KEY = token
+
+    canvas = Canvas(API_URL, API_KEY)
+    return canvas
+
+
+'''Get submissions'''
+def get_submissions(canvas, course_id, assignment_id):
+    course = canvas.get_course(course_id)
+    assignment = course.get_assignment(assignment_id)
+    submissions = assignment.get_submissions()
+    return submissions
+
+
+if __name__=="__main__":
+    '''Specify course and assignment id'''
+    course_id = 51824
+    assignment_id = 1047215
+    path_to_files = "./marked_files/"
+
+    canvas = connect_to_canvas()
+
+    '''Initialize dictionaries'''
+    marked_files_dict = create_marked_files_dict(path_to_files)
+    submission_dict = create_submissions_dict(canvas, course_id, assignment_id)
+
+    '''Upload files as comments on submissions'''
+    for stu_id in marked_files_dict:
+        if stu_id in submission_dict:
+            (is_success, json) = submission_dict[stu_id].upload_comment(marked_files_dict[stu_id])
+            if is_success:
+                print(f'File successfully uploaded to submission {submission_dict[stu_id]} for student {stu_id}.')
+            else:
+                print(f'ERROR: Failed for submission {submission_dict[stu_id]} for student {stu_id}\n. {json}')
